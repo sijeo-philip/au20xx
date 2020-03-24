1a0eea663f9d94d4da6a289bf9d6727ec6d1442d
Sijeo Philip <sijeo80@gmail.com>
2020-03-25 01:07:41 +0530

    Started Adding ADC Configuration Bits...

---
+++ au20xx_flow_sensor/interface/hal_adc.h
@@ -0,0 +1,15 @@
+/*
+ * hal_adc.h
+ *
+ *  Created on: Mar 24, 2020
+ *      Author: Hp
+ */
+
+#ifndef INTERFACE_HAL_ADC_H_
+#define INTERFACE_HAL_ADC_H_
+
+
+
+
+
+#endif /* INTERFACE_HAL_ADC_H_ */
